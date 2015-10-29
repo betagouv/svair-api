7f4f705b0f592c87206ea03dcec7794eb9e02968
Thibaut Géry <thibaut.gery@gmail.com>
2015-10-29 11:56:41 +0100

    parse all the data

--- test/parse.test.js
+++ test/parse.test.js
@@ -16,8 +16,13 @@ describe('Parse ', function () {
     it("remove € symbol", function () {
       expect(parseEuro("13000€")).to.be.equal(13000)
     })
+
+    it("remove non breakable space symbol", function () {
+      expect(parseEuro("13\u00A0000")).to.be.equal(13000)
+    })
+
     it("remove space € symbol", function () {
-      expect(parseEuro("13 000 €")).to.be.equal(13000)
+      expect(parseEuro("1 665 €\n\t\t\t\t\t")).to.be.equal(1665)
     })
   });
 
@@ -47,6 +52,86 @@ describe('Parse ', function () {
           prenoms: '',
           dateNaissance: ''
         });
+        done();
+      })
+    })
+
+    it("extract the part number", function (done) {
+      parseResult(postHttpResponse, function(err, result) {
+        if(err) return done(err);
+        expect(result.nombreParts).to.equal(1)
+        done();
+      })
+    })
+
+    it("extract the dateRecouvrement ", function (done) {
+      parseResult(postHttpResponse, function(err, result) {
+        if(err) return done(err);
+        expect(result.dateRecouvrement).to.equal("31/07/2015")
+        done();
+      })
+    })
+
+    it("extract the dateEtablissement ", function (done) {
+      parseResult(postHttpResponse, function(err, result) {
+        if(err) return done(err);
+        expect(result.dateEtablissement).to.equal("08/07/2015")
+        done();
+      })
+    })
+
+    it("extract the situationFamille ", function (done) {
+      parseResult(postHttpResponse, function(err, result) {
+        if(err) return done(err);
+        expect(result.situationFamille).to.equal("Célibataire")
+        done();
+      })
+    })
+
+    it("extract the nombrePersonnesCharge ", function (done) {
+      parseResult(postHttpResponse, function(err, result) {
+        if(err) return done(err);
+        expect(result.nombrePersonnesCharge).to.equal(0)
+        done();
+      })
+    })
+
+    it("extract the revenuBrutGlobal ", function (done) {
+      parseResult(postHttpResponse, function(err, result) {
+        if(err) return done(err);
+        expect(result.revenuBrutGlobal).to.equal(17580)
+        done();
+      })
+    })
+
+    it("extract the revenuImposable ", function (done) {
+      parseResult(postHttpResponse, function(err, result) {
+        if(err) return done(err);
+        expect(result.revenuImposable).to.equal(17580)
+        done();
+      })
+    })
+
+    it("extract the impotRevenuNetAvantCorrections ", function (done) {
+      parseResult(postHttpResponse, function(err, result) {
+        if(err) return done(err);
+        expect(result.impotRevenuNetAvantCorrections).to.equal(1665)
+        done();
+      })
+    })
+
+    it("extract the montantImpot ", function (done) {
+      parseResult(postHttpResponse, function(err, result) {
+        if(err) return done(err);
+        expect(result.montantImpot).to.equal(1665)
+        done();
+      })
+    })
+
+    it("extract the revenuFiscalReference ", function (done) {
+      parseResult(postHttpResponse, function(err, result) {
+        if(err) return done(err);
+        expect(result.revenuFiscalReference).to.equal(17580)
         done();
       })
     })
--- utils/parse.js
+++ utils/parse.js
@@ -2,11 +2,16 @@
 var _ = require('lodash');
 var jsdom = require("jsdom");
 
-
-
-module.exports.euro = function parseEuro(str) {
-    return _.parseInt(str.replace(/€/g, '').replace(/\ /g, ''));
+function parseEuro(str) {
+  return _.parseInt(str
+    .replace(/\u00A0/g, '')
+    .replace(/€/g, '')
+    .replace(/ /g, '')
+    .replace(/\n/g, '')
+    .replace(/\t/g, ''));
 }
+
+module.exports.euro = parseEuro
 
 
 module.exports.result = function parseResult(html, callback) {
@@ -23,6 +28,27 @@ module.exports.result = function parseResult(html, callback) {
   };
 
 
+  var mapping = {
+    dateRecouvrement: 'Date de mise en recouvrement de l\'avis d\'impôt',
+    dateEtablissement: 'Date d\'établissement',
+    nombreParts: { src: 'Nombre de part(s)', fn: parseFloat },
+    situationFamille: 'Situation de famille',
+    nombrePersonnesCharge: { src: 'Nombre de personne(s) à charge', fn: _.parseInt },
+    revenuBrutGlobal: { src: 'Revenu brut global', fn: parseEuro },
+    revenuImposable: { src: 'Revenu imposable', fn: parseEuro },
+    impotRevenuNetAvantCorrections: { src: 'Impôt sur le revenu net avant corrections', fn: parseEuro },
+    montantImpot: { src: 'Montant de l\'impôt', fn: parseEuro },
+    revenuFiscalReference: { src: 'Revenu fiscal de référence', fn: parseEuro }
+};
+
+var compactedMapping = _.map(mapping, function (val, key) {
+    var obj = _.isString(val) ? { src: val } : val;
+    return _.assign(obj, { dest: key });
+});
+
+var mappingBySrc = _.indexBy(compactedMapping, 'src');
+
+
   jsdom.env(
     html,
     ["http://code.jquery.com/jquery.js"],
@@ -35,6 +61,13 @@ module.exports.result = function parseResult(html, callback) {
           var key = mappingDeclarant[rowHeading];
           result.declarant1[key] = cells.eq(1).text().trim()
           result.declarant2[key] = cells.eq(2).text().trim()
+        } else if (cells.length === 2 && rowHeading in mappingBySrc) {
+            var mappingEntry = mappingBySrc[rowHeading];
+            if (mappingEntry.fn) {
+              result[mappingEntry.dest] = mappingEntry.fn(cells.eq(1).text());
+            } else {
+              result[mappingEntry.dest] = cells.eq(1).text().trim();
+            }
         }
       })
       callback(null, result)
